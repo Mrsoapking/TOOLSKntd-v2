99f4db435478358f4b2e1693ae9c26473ddb049d
VinzSec <49777000+Vinzid444@users.noreply.github.com>
2019-05-12 10:43:47 +0700

    Add files via upload

---
+++ new.sh
@@ -0,0 +1,139 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+toilet -f big TOOLSKntd -F gay
+
+echo $bi"[•]_____________________________________________[•]"
+echo  $pur"|   Author : Vinz [Gans]                         |"
+echo  $cy"| Creadby : Vinz Gans                            |"
+echo  $me"| Contact : +6281553032207                       |"
+echo  $pu"| Thanks To : All Member Indonesia Termux Lite   |"
+echo $ku"[•]_____________________________________________[•]"
+
+echo $ku" __________________________________________"
+echo $i" |1. BRUTE FORCE Iqbalz                    |"
+echo $i" |2. BRUTE FORCEnew                        |"
+echo $i" |3. Tools AutoGanteng V2                  |"
+echo $i" |4. Auto_Deface                           |"
+echo $i" |5. BRUTAL SMS                            |"
+echo $i" |6. TOOLS KONTOOLS                        |"
+echo $i" |7. TOOLS INSTALLER V1                    |"
+echo $i" |8. HAMMER DDOS                           |"
+echo $i" |9. DDOS_V1                               |"
+echo $i" |10. LUCITA DDOS                          |"
+echo $i" |11. Mass Webdav                          |"
+echo $i" |12. Termux-Deface                        |"
+echo $i" |0 . Exit Dari Tools Ini:)                |"
+echo $ku" __________________________________________"
+
+echo $pur"CTRL+C UNTUK KELUAR :)"
+
+echo $me"┌==="$bi"["$i"VinzGans"$bi"]"$me"======"$bi"["$i""Pilih Nomornya""$bi"]"
+echo $me"¦"
+read -p"└──➲ " pil
+
+if [ $pil = 1 ]
+then
+clear
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+pkg install python2
+python2 brute.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+git clone https://github.com/FR13ND8/BRUTEFORCEnew
+cd BRUTEFORCEnew
+sh new.sh
+fi
+
+if [ $pil = 3 ]
+then
+clear
+git clone https://github.com/Vinzid444/tools-autogantengV2
+cd tools-autogantengV2
+sh tools-autogantengV2.sh
+fi
+
+if [ $pil = 4 ]
+then
+clear
+git clone https://github.com/1ND0T3RML1T3/Auto_Deface
+cd Auto_Deface
+pkg install python2
+sh AutoDeface.sh
+fi
+
+if [ $pil = 5 ]
+then
+clear
+git clone https://github.com/N74NK/brutalsms
+cd brutalsms
+python2 brutalsms.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+git clone https://github.com/DarkCurut08/KONTOOLS
+cd KONTOOLS
+sh Moreno77.sh
+fi
+
+if [ $pil = 7 ]
+then
+clear
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv1
+cd TOOLSINSTALLERv1
+sh Tuanb4dut.sh
+fi
+
+if [ $pil = 8 ]
+then
+clear
+git clone https://github.com/cyweb/hammer
+cd hammer
+ls
+
+
+if [ $pil = 9 ]
+git clone https://github.com/1ND0T3RML1T3/DDOS_V1
+cd DDOS_V1
+python2 DDos.py
+fi
+
+if [ $pil = 10 ]
+then
+clear
+git clone https://github.com/zlucifer/lucita_ddos
+cd lucita_ddos
+ls
+
+if [ $pil = 11 ]
+then
+clear
+git clone https://github.com/N74NK/mass-webdav
+cd mass-webdav
+python masswebdav.py
+fi
+
+if [ $pil = 12 ]
+then
+clear
+git clone https://github.com/wahyuandhika/termux-deface
+cd termux-deface
+python2 termux-deface.py
+if
+
+if [ $pil = 0 ]
+then
+exit
+fi
